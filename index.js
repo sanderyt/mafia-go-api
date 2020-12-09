b94ae3436794492c0aa1155d1305a3a2879d328f
Sander van Rijsoort <hallo@svanrijsoort.nl>
2020-12-09 19:01:57 +0000

    made change to port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
