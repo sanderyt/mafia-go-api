3cc30e3a3b9d6d58357cee240dfa8a9f2def6e19
Sander van Rijsoort <hallo@svanrijsoort.nl>
2020-12-09 19:00:42 +0000

    deploy commit

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the Mafia Go API");
+  res.send("Welcome to the Mafia Go API!");
 });
 
 app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`));
