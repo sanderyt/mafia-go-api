1fa4a1dd7616b6da3512da0bc04a281600975bf0
Sander van Rijsoort <hallo@svanrijsoort.nl>
2020-12-12 14:29:26 +0000

    changed ip to mapboxid

--- models/Property.js
+++ models/Property.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const propertySchema = new mongoose.Schema({
-  id: {
+  mapboxId: {
     type: String,
     required: true
   },
