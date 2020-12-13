73b7529ffb12a44b74e30f0bd0d5bacfbf0ee46c
Sander van Rijsoort <hallo@svanrijsoort.nl>
2020-12-13 14:40:03 +0000

    a small change

--- controllers/users.js
+++ controllers/users.js
@@ -7,7 +7,7 @@ module.exports = {
   getAllUsers: async (req, res, next) => {
     try {
       const users = await User.find();
-      res.status(200).json({ users });
+      res.status(200).json(users);
     } catch (error) {
       res.status(400).send(error);
     }
